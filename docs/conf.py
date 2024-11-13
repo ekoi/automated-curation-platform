9bfbad9822fd03db88b2ae32c7fb7624a9d1db6a
ekoi <eko.indarto@dans.knaw.nl>
2024-11-13 10:47:15 +0100

    update index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -31,5 +31,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
-master_doc = 'index'
+html_static_path = ['_static']
+# master_doc = 'index'
