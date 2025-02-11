f4e0eb34bd62dd3ab04b87fc9b35c480d09f4a35
ekoi <eko.indarto@dans.knaw.nl>
2025-02-11 14:40:32 +0100

    Replace SWHID snapshot to dir

--- src/plugins/swh_api_depositor.py
+++ src/plugins/swh_api_depositor.py
@@ -51,9 +51,9 @@ class SwhApiDepositor(Bridge):
             while True and (counter < settings.SWH_API_MAX_RETRIES):
                 counter += 1
                 swh_check_url = api_resp_json.get("request_url")
-                check_resp = requests.get(swh_check_url, headers=headers)
-                if check_resp.status_code == 200:
-                    swh_resp_json = check_resp.json()
+                step1_check_resp = requests.get(swh_check_url, headers=headers)
+                if step1_check_resp.status_code == 200:
+                    swh_resp_json = step1_check_resp.json()
                     logger(f'{swh_check_url} response: {json.dumps(swh_resp_json)}', settings.LOG_LEVEL, self.app_name)
                     if swh_resp_json.get('save_task_status') == DepositStatus.FAILED:
                         tdm.deposit_status = DepositStatus.FAILED
@@ -62,19 +62,60 @@ class SwhApiDepositor(Bridge):
                         target_response.content_type = ResponseContentType.JSON
                         break
                     elif swh_resp_json.get('snapshot_swhid'):
+                        logger(f"snapshot_swhid: {swh_resp_json.get('snapshot_swhid')}.", settings.LOG_LEVEL,  self.app_name)
+
+                        snapshot_url = swh_resp_json.get('snapshot_url')
+                        # Request the snapshot url
+                        logger(f"Get request to : {snapshot_url}.", settings.LOG_LEVEL, self.app_name)
+                        step2_snapshot_resp = requests.get(snapshot_url, headers=headers)
+                        if step2_snapshot_resp.status_code != 200:
+                            logger(f"snapshot_resp.status_code: {step2_snapshot_resp.status_code}", "error",
+                                   self.app_name)
+                            tdm.deposit_status = DepositStatus.ERROR
+                            target_response.status_code = step2_snapshot_resp.status_code
+                            target_response.content_type = ResponseContentType.JSON
+                            target_response.content = json.dumps(step2_snapshot_resp.json())
+                            break
+                        snapshot_resp_json = step2_snapshot_resp.json()
+                        logger(f"snapshot_resp_json: {json.dumps(snapshot_resp_json)}", settings.LOG_LEVEL,
+                               self.app_name)
+                        target_value = snapshot_resp_json['branches']['HEAD']['target']
+                        logger(f"branches-HEAD-target_value: {target_value}", settings.LOG_LEVEL, self.app_name)
+                        # Get branches - refs/heads/master
+                        target_url = snapshot_resp_json['branches'][target_value]['target_url']
+                        logger(f"Request to branches-{target_value}-target_url: {target_url}", settings.LOG_LEVEL, self.app_name)
+                        step3_master_branch_resp = requests.get(target_url, headers=headers)
+                        logger(f"master_branch_resp.status_code: {step3_master_branch_resp.status_code}", settings.LOG_LEVEL, self.app_name)
+                        if step3_master_branch_resp.status_code != 200:
+                            logger(f"master_branch_resp.status_code: {step3_master_branch_resp.status_code}", "error",
+                                   self.app_name)
+                            tdm.deposit_status = DepositStatus.ERROR
+                            target_response.status_code = step3_master_branch_resp.status_code
+                            target_response.content_type = ResponseContentType.JSON
+                            target_response.content = json.dumps(step3_master_branch_resp.json())
+                            break
+
+                        master_branch_resp_json = step3_master_branch_resp.json()
+                        logger(f"master_branch_resp_json: {json.dumps(master_branch_resp_json)}", settings.LOG_LEVEL,
+                               self.app_name)
+                        swhid_dir = f'swh:1:dir:{master_branch_resp_json['directory']}'
+                        swhid_dir_url = master_branch_resp_json['directory_url']
+                        logger(f"SWHID_DIR: {swhid_dir} with URL: {swhid_dir_url}", settings.LOG_LEVEL, self.app_name)
+
                         tdm.deposit_status = DepositStatus.FINISH
-                        target_response.status_code = check_resp.status_code
+                        target_response.status_code = step1_check_resp.status_code
                         target_response.content_type = ResponseContentType.JSON
-                        target_response.content = json.dumps(swh_resp_json)
+                        target_response.content = json.dumps(master_branch_resp_json)
                         identifier_items = []
                         target_response.identifiers = identifier_items
-                        ideni = IdentifierItem(value=swh_resp_json.get('snapshot_swhid'), url=swh_url,
+                        ideni = IdentifierItem(value=swhid_dir, url=swhid_dir_url,
                                                protocol=IdentifierProtocol('swhid'))
                         identifier_items.append(ideni)
                         break
                     else:
                         goto_sleep = True
                 if goto_sleep:
+                    logger(f'goto_sleep: {goto_sleep}', settings.LOG_LEVEL, self.app_name)
                     sleep(settings.SWH_DELAY_POLLING)
 
         else:
